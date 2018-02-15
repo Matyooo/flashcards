7947365a2adb91a6204222a2ec598afb0941ffb4
Matyooo <matyooo@gmail.com>
2018-02-15 12:39:36 +0100

    App modified

--- flashcardClient/App.js
+++ flashcardClient/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, TextInput, View, Button } from 'react-native';
 
-export default class App extends React.Component {
+export default class HelloWorldApp extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+
+     <View style={{padding: 10}}>
+      <Text>Hello world view!</Text>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+        />
+             <Button
+  onPress={() => {
+    Alert.alert('You tapped the button!');
+  }}
+  title="Press Me"
+	/>
+
+     </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
